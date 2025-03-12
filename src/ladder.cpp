876dc67037e96e6fac052a13a9a011b2ce671758
aariela <abaincia@uci.edu>
2025-03-12 03:15:06 +0000

    generate

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -38,7 +38,62 @@ bool is_adjacent(const string& word1, const string& word2) {
     return edit_distance_within(word1, word2, 1);
 }
 
-vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+// vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
+//     if (begin_word == end_word) {
+//         error(begin_word, end_word, "Start and end words are the same");
+//         return {};
+//     }
+
+//     if (word_list.find(end_word) == word_list.end()) {
+//         error(begin_word, end_word, "End word not in dictionary");
+//         return {};
+//     }
+
+//     unordered_set<string> dict(word_list.begin(), word_list.end());
+//     queue<vector<string>> q;
+//     q.push({begin_word});
+//     unordered_set<string> visited;
+//     visited.insert(begin_word);
+
+//     while (!q.empty()) {
+//         int qSize = q.size();
+//         unordered_set<string> this_level_visited;
+
+//         for (int i = 0; i < qSize; ++i) {
+//             vector<string> path = q.front(); q.pop();
+//             string last_word = path.back();
+
+//             for (int j = 0; j < last_word.size(); ++j) {
+//                 string new_word = last_word;
+//                 for (char c = 'a'; c <= 'z'; ++c) {
+//                     if (c == last_word[j]) continue;
+//                     new_word[j] = c;
+
+//                     if (dict.find(new_word) == dict.end()) continue;
+
+//                     if (new_word == end_word) {
+//                         path.push_back(new_word);
+//                         return path;
+//                     }
+
+//                     if (visited.find(new_word) == visited.end()) {
+//                         vector<string> new_path = path;
+//                         new_path.push_back(new_word);
+//                         q.push(new_path);
+//                         this_level_visited.insert(new_word);
+//                     }
+//                 }
+//             }
+//         }
+
+//         for (const auto& word : this_level_visited)
+//             visited.insert(word);
+//     }
+
+//     return {};
+// }
+
+ vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list) {
     if (begin_word == end_word) {
         error(begin_word, end_word, "Start and end words are the same");
         return {};
@@ -49,81 +104,44 @@ vector<string> generate_word_ladder(const string& begin_word, const string& end_
         return {};
     }
 
-    unordered_set<string> dict(word_list.begin(), word_list.end());
-    queue<vector<string>> q;
-    q.push({begin_word});
-    unordered_set<string> visited;
+    queue<vector<string>> ladder_queue;
+    set<string> visited;
+    ladder_queue.push({begin_word});
     visited.insert(begin_word);
 
-    while (!q.empty()) {
-        int qSize = q.size();
-        unordered_set<string> this_level_visited;
+    while (!ladder_queue.empty()) {
+        int level_size = ladder_queue.size();
+        set<string> this_level_visited;
 
-        for (int i = 0; i < qSize; ++i) {
-            vector<string> path = q.front(); q.pop();
-            string last_word = path.back();
+        for (int i = 0; i < level_size; ++i) {
+            vector<string> current_ladder = ladder_queue.front();
+            ladder_queue.pop();
 
-            for (int j = 0; j < last_word.size(); ++j) {
-                string new_word = last_word;
-                for (char c = 'a'; c <= 'z'; ++c) {
-                    if (c == last_word[j]) continue;
-                    new_word[j] = c;
+            string last_word = current_ladder.back();
 
-                    if (dict.find(new_word) == dict.end()) continue;
+            for (const string& word : word_list) {
+                if (visited.count(word) || !is_adjacent(last_word, word)) continue;
 
-                    if (new_word == end_word) {
-                        path.push_back(new_word);
-                        return path;
-                    }
+                vector<string> new_ladder = current_ladder;
+                new_ladder.push_back(word);
 
-                    if (visited.find(new_word) == visited.end()) {
-                        vector<string> new_path = path;
-                        new_path.push_back(new_word);
-                        q.push(new_path);
-                        this_level_visited.insert(new_word);
-                    }
+                if (word == end_word) {
+                    return new_ladder;
                 }
+
+                ladder_queue.push(new_ladder);
+                this_level_visited.insert(word);  // mark it only for this level
             }
         }
 
-        for (const auto& word : this_level_visited)
+        // After exploring the entire level, now mark words as visited
+        for (const string& word : this_level_visited) {
             visited.insert(word);
+        }
     }
 
     return {};
 }
-
-    // queue<vector<string>> ladder_queue;
-    // set<string> visited;
-
-    // ladder_queue.push({begin_word});
-    // visited.insert(begin_word);
-
-    // while (!ladder_queue.empty()) {
-    //     vector<string> ladder = ladder_queue.front();
-    //     ladder_queue.pop();
-
-    //     string last_word = ladder.back();
-
-    //     for (const string& word : word_list) {
-    //         if (visited.find(word) != visited.end()) continue;
-    //         if (is_adjacent(last_word, word)) {
-    //             if (visited.find(word) == visited.end()) {
-    //                 visited.insert(word);
-    //                 vector<string> new_ladder = ladder;
-    //                 new_ladder.push_back(word);
-
-    //                 if (word == end_word) {
-    //                     return new_ladder;
-    //                 }
-
-    //                 ladder_queue.push(new_ladder);
-    //             }
-    //         }
-    //     }
-    // }
-
-    // return {};
 
 
 void load_words(set<string> & word_list, const string& file_name) {
